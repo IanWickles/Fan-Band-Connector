e198b9f6146c2f3de539780bd1a1d6aa3ae2aa77
Ian Wickles <ian.wickles@gmail.com>
2022-08-08 15:46:58 -0400

    Made 8 tables and added 3 users

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role,email_address) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER','user@gmail.com');
+INSERT INTO users (username,password_hash,role,email_address) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', 'admin@gmail.com');
+INSERT INTO users (username,password_hash,role,email_address) VALUES ('manager','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_MANAGER', 'manager@gmail.com');
 
 COMMIT TRANSACTION;
