4ef6ef6330109042cb218ad847012cf95809e756
Nichole Zacherl <nlz16214@gmail.com>
2022-08-09 11:41:05 -0400

    created NewBandForm template

--- capstone/vue/src/components/NewBandForm.vue
+++ capstone/vue/src/components/NewBandForm.vue
@@ -1,4 +1,12 @@
-<template> </template>
+<template>
+  <form class="new-band-form">
+    <input class="band-name-input" type="text" placeholder="Band Name" />
+    <input class="description-input" type="text" placeholder="" />
+    <input class="" type="text" placeholder="" />
+    <input class="" type="" placeholder="" />
+    <input class="" type="" placeholder="" />
+  </form>
+</template>
 
 <script>
 export default {};
