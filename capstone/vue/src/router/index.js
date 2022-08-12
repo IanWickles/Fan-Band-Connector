b2357c11a7080be4bb1bf6700fb8da0932f672c9
Nichole Zacherl <nlz16214@gmail.com>
2022-08-12 13:06:08 -0400

    working new-band-form, router link to band pages

--- capstone/vue/src/router/index.js
+++ capstone/vue/src/router/index.js
@@ -84,7 +84,7 @@ const router = new Router({
     //   }
     // },
     {
-      path: "/new-band",
+      path: "/bands/new-band",
       name: "new-band",
       component: NewBand,
       meta: {
