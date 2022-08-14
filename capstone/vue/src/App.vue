9331bd57b6fe449f64fe37de8e74dcd81382677a
Stefanie McCook <some1udk@yahoo.com>
2022-08-14 04:03:12 -0400

    slowly looking prettier and fixed the is this person logged in or not issue

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -2,26 +2,71 @@
 
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link> |
-      <router-link v-bind:to="{ name: 'bands' }">Bands</router-link> |
-      <router-link v-bind:to="{ name: 'venues' }">Venues</router-link> |
-      <router-link v-bind:to="{ name: 'profile' }">Profile</router-link> |
-      <router-link
-        v-bind:to="{ name: 'login' }"
-        v-if="($store.state.token = 'token')"
-        >Login</router-link
-      >
-      <router-link
-        v-bind:to="{ name: 'logout' }"
-        v-if="$store.state.token != 'token'"
-        >Logout</router-link>
+    <div id="titleBar">
+      <img src="header.png"/>
     </div>
-    <router-view />
+    <div id="nav" class="navbar is-pulled-right" :class="{ collapsed: navbarCollapsed }">
+      <div class="collapse-bar" @click="toggleNavBar">
+        <i class="fa-solid" :class="{ 'fa-caret-right': !navbarCollapsed, 'fa-caret-left': navbarCollapsed }"></i>
+      </div>
+      <ul>
+        <li class="navbar-item"><router-link v-bind:to="{ name: 'home' }"><i class="fa-solid fa-house"></i> <div>Home</div></router-link></li>
+        <li class="navbar-item"><router-link v-bind:to="{ name: 'bands' }"><i class="fa-solid fa-guitar"></i> <div>Bands</div></router-link></li>
+        <li class="navbar-item"><router-link v-bind:to="{ name: 'venues' }"><i class="fa-solid fa-building"></i> <div>Venues</div></router-link></li>
+        <li class="navbar-item" v-if="loggedIn"><router-link v-bind:to="{ name: 'profile' }"><i class="fa-solid fa-user"></i> <div>Profile</div></router-link></li>
+        <li class="navbar-item" v-if="loggedIn"><router-link v-bind:to="{ name: 'logout' }"><i class="fa-solid fa-right-from-bracket"></i> <div>Logout</div></router-link></li>
+        <li class="navbar-item" v-else><router-link v-bind:to="{ name: 'login' }"><i class="fa-solid fa-right-to-bracket"></i> <div>Login</div></router-link></li>
+      </ul>
+    </div>
+    <div id="content">
+      <router-view />
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        navbarCollapsed: false
+      };
+    },
+    methods: {
+      toggleNavBar() {
+        this.navbarCollapsed = !this.navbarCollapsed;
+      }
+    },
+    computed: {
+      loggedIn() {
+        return this.$store.state.token != '';
+      }
+    }
+  }
+</script>
+
+
 <style>
   #nav {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    align-items: center;
+  }
+  .navbar.collapsed li div {
+    display: none;
+  }
+  .navbar .navbar-item a {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    align-items: center;
+  }
+  .collapse-bar {
+    cursor: pointer;
+  }
+  #titleBar {
+    height: 150px;
     text-align: right;
+  }
+  #titleBar img {
+    height: 95%;
   }
 </style>
--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <h1>Welcome to Louisiana Beat-Wave!</h1><br><br>
+    <p class="mymessage">{{ message }}</p><br><br>
+    <button><router-link v-bind:to="{ name: 'bands' }">Explore Bands</router-link></button>
+    <button></button>
+    <button></button>
+    <button></button>
   </div>
 </template>
 
@@ -11,5 +15,20 @@ home page
 <script>
 export default {
   name: "home",
-};
+  data() {
+    return {
+      message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu mi bibendum neque egestas. A diam sollicitudin tempor id eu nisl nunc. Sed lectus vestibulum mattis ullamcorper velit. Sed odio morbi quis commodo. Malesuada fames ac turpis egestas integer eget. Cras ornare arcu dui vivamus. Volutpat ac tincidunt vitae semper quis lectus. Nulla facilisi morbi tempus iaculis. Sem nulla pharetra diam sit amet nisl. Purus ut faucibus pulvinar elementum integer enim. Nisl purus in mollis nunc sed id semper. Eget nulla facilisi etiam dignissim diam quis enim. Sed tempus urna et pharetra pharetra massa massa. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Habitasse platea dictumst quisque sagittis. Consectetur purus ut faucibus pulvinar."
+    }
+  }
+}
 </script>
+<style>
+h1 {
+  text-align: center;
+  font-size: xx-large;
+}
+.mymessage {
+  text-align: center;
+  font-family: 'Indie Flower', cursive;
+}
+</style>
