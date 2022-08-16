43ca934bcc3da59d12b2b6e6bfd8f4a6fa9120c2
Patrick Byrne <ptbyrne13@gmail.com>
2022-08-16 15:57:44 -0400

    to merge

--- capstone/vue/src/components/BandDetails.vue
+++ capstone/vue/src/components/BandDetails.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <div>
+  <b-button v-b-modal.modal-1>Send Message</b-button>
+
+  <b-modal id="modal-1" title="BootstrapVue">
+    <p class="my-4">Hello from modal!</p>
+  </b-modal>
+</div>
     <h1 class="name">{{ this.$store.state.activeBand.bandName }}</h1>
     <div class="managerhub">
     <button class="manageredit">Edit</button> |
